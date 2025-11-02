798f63219b6f928e172a47d75ae9358868098339
pranav2003glitch <patilpranav0217@gmail.com>
2025-11-02 14:35:36 +0530

    vercel file added

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import app from './app.js';
 config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 const server = app.listen(PORT, () => {
   console.log('🚀 Server started successfully');
